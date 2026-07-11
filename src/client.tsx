37eddbd5b388a0ea5453d06aa7825ce71ee8383f
bigdatyagi <159639354+bigdatyagi@users.noreply.github.com>
2026-07-11 14:08:04 +0530

    fix: remove invalid router prop from StartClient and format file

--- src/client.tsx
+++ src/client.tsx
@@ -1,15 +1,9 @@
 /// <reference types="vite/client" />
 import { StartClient } from "@tanstack/react-start/client";
 import { createRoot, hydrateRoot } from "react-dom/client";
-import { getRouter } from "./router";
-
-const router = getRouter();
 
 const rootElement = document;
 
 if (!rootElement.getElementById("root-hydrated")) {
-  hydrateRoot(
-    rootElement,
-    <StartClient router={router} />
-  );
+  hydrateRoot(rootElement, <StartClient />);
 }
